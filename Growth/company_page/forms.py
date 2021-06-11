e3a0d83ab9721185e29e8e09174cf6c871c62e26
theRealEthanZhang <zhangzhengyi@Ethan-Zhang-MacBook-Pro.local>
2021-06-11 17:04:32 -0400

    Add dynamic url to company; add company redirecting in

--- Growth/company_page/forms.py
+++ Growth/company_page/forms.py
@@ -1,8 +1,37 @@
 from django import forms
+from django.utils.safestring import mark_safe
+
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Company
 
+
 class AddCompanyForm(forms.ModelForm):
+    def __init__(self,*args, **kwargs):
+        super(AddCompanyForm, self).__init__(*args, **kwargs)
+
+        self.fields['industry'].label = "Industry*"
+        self.fields['size'].label = "Size*"
+        self.fields['type'].label = "Type*"
+        self.fields['website_url'].help_text = \
+            mark_safe('<br><i>URL should start with http:// or https:// or www.</i>')
+        self.fields['verify'].required = True
+        self.fields['verify'].label = ""
+
+
+    name = forms.CharField(label=mark_safe('<h5>Company Name*</h5>'), label_suffix='')
+    description = forms.CharField(
+        label='Description*',
+        label_suffix=mark_safe('<br><br'),
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "A brief introduction of your company's nature of business",
+                "rows":5,
+                "cols":60
+            }
+        )
+    )
+
     class Meta:
         model = Company
         fields = [
@@ -14,9 +43,32 @@ class AddCompanyForm(forms.ModelForm):
             'location',
             'website_url',
             'logo',
+            'verify'
         ]
 
+
+
 class ModifyCompanyForm(forms.ModelForm):
+    def __init__(self,*args, **kwargs):
+        super(ModifyCompanyForm, self).__init__(*args, **kwargs)
+
+        self.fields['industry'].label = "Industry*"
+        self.fields['size'].label = "Size*"
+        self.fields['type'].label = "Type*"
+
+    name = forms.CharField(label=mark_safe('<h5>Company Name*</h5>'), label_suffix='')
+    description = forms.CharField(
+        label='Description*',
+        label_suffix=mark_safe('<br><br'),
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "A brief introduction of your company's nature of business",
+                "rows":5,
+                "cols":60
+            }
+        )
+    )
+
     class Meta:
         model = Company
         fields = [
