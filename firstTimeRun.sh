46ddf7e86a2635682738e3835a6add001e527fee
SahotaJai <jaiteg.sahota@outlook.com>
2021-06-11 23:38:38 -0400

    Revert "Added django-channels install to firstTimeRun.sh"
    This reverts commit 1e3221c97dc405cdd104e281e3bc978d0be1f453.

--- firstTimeRun.sh
+++ firstTimeRun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in 'virtualenv -p python3 .' 'source bin/activate' 'pip install django' 'pip install django-crispy-forms' 'pip install pillow' 'pip install django-channels' 'cd Growth' 'python3 manage.py runserver'
+for f in 'virtualenv -p python3 .' 'source bin/activate' 'pip install django' 'pip install django-crispy-forms' 'pip install pillow' 'cd Growth' 'python3 manage.py runserver'
 do
   $f
   rc=$?
