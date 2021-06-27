eca0d0fbd025d4cbaf405473f87708a83ea337d5
NamanBhandari26 <81213025+NamanBhandari26@users.noreply.github.com>
2021-06-27 23:33:22 +0530

    Changed Roles Pattern back to Select

--- Growth/users/forms.py
+++ Growth/users/forms.py
@@ -6,4 +6,4 @@ from .models import User
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ("username", "email")
+        fields = ("username", "email", "role")
