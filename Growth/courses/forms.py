ba406ec6ac31224c4730f6a8a4158dc24cc85bbc
cydy8001 <cydy8001@gmail.com>
2021-06-29 13:37:42 -0400

    GROW-37
    Finished GROW-37 all stuff

--- Growth/courses/forms.py
+++ Growth/courses/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
-from courses.models import CourseInfo
+from courses.models import CourseInfo, CourseUser
 
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = CourseInfo
         fields = ('name', 'description')
+
--- Growth/courses/models.py
+++ Growth/courses/models.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from django.db import models
 
 # Create your models here.
+from users.models import User
+
+
 class CourseInfo(models.Model):
     #image = models.ImageField(upload_to='course/',)
     id = models.BigAutoField(primary_key=True)
@@ -15,6 +18,12 @@ class CourseInfo(models.Model):
     class Meta:
         verbose_name = "coursename"
         verbose_name_plural = verbose_name
+
+
+class CourseUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(CourseInfo, on_delete=models.CASCADE)
+
 
 class LessonInfo(models.Model):
     name = models.CharField(max_length=50, verbose_name="chaptername")
@@ -29,31 +38,3 @@ class LessonInfo(models.Model):
         verbose_name_plural = verbose_name
 
 
-class VideoInfo(models.Model):
-    name = models.CharField(max_length=50, verbose_name="videoname")
-    study_time = models.IntegerField(default=0, verbose_name="study_time")
-    url = models.URLField(default='https://www.utoronto.ca/',verbose_name="movie link",max_length=200)
-    lessoninfo = models.ForeignKey(LessonInfo, on_delete=models.CASCADE, verbose_name="lessoninfo")
-    add_time = models.DateTimeField(default=datetime.now,verbose_name="add time" )
-
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-
-        verbose_name = "video_name"
-        verbose_name_plural = verbose_name
-
-class SourceInfo(models.Model):
-    name = models.CharField(max_length=50, verbose_name="resourcename")
-    downloads = models.FileField(upload_to='source/',max_length=200,verbose_name="downloadpath")
-    add_time = models.DateTimeField(default=datetime.now,verbose_name="add time" )
-    course_info = models.ForeignKey(CourseInfo,on_delete=models.CASCADE,verbose_name="Course")
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "resource"
-        verbose_name_plural = verbose_name
