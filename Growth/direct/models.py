9946af40f0e49c2b1ac057971e56f80f770fdfd3
cydy8001 <cydy8001@gmail.com>
2021-06-30 20:49:20 -0400

    GROW-46
    Direct stuff

--- Growth/direct/models.py
+++ Growth/direct/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Max
-
+from Growth import settings
 # Create your models here.
 class Message(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-	sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
-	recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user')
+	sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='from_user')
+	recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='to_user')
 	body = models.TextField(max_length=1000, blank=True, null=True)
 	date = models.DateTimeField(auto_now_add=True)
 	is_read = models.BooleanField(default=False)
