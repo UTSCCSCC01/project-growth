0e12593d6fd2588a139efd633dc624b4214b4ae8
NamanBhandari26 <81213025+NamanBhandari26@users.noreply.github.com>
2021-07-01 16:13:23 +0530

    GROW40: Connectivity of Upload and Course Started Working

--- Growth/courses/views.py
+++ Growth/courses/views.py
@@ -141,7 +141,7 @@ def unenrollCourse(request):
 
 
 class Home(TemplateView):
-    template_name = 'book_list.html'
+    template_name = 'courses/book_list.html'
 
 
 def upload(request):
@@ -151,12 +151,12 @@ def upload(request):
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
-    return render(request, 'upload.html', context)
+    return render(request, 'courses/upload.html', context)
 
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'book_list.html', {
+    return render(request, 'courses/book_list.html', {
         'books': books
     })
 
@@ -166,10 +166,10 @@ def upload_book(request):
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('courses/book_list')
     else:
         form = BookForm()
-    return render(request, 'upload_book.html', {
+    return render(request, 'courses/upload_book.html', {
         'form': form
     })
 
@@ -178,17 +178,17 @@ def delete_book(request, pk):
     if request.method == 'POST':
         book = Book.objects.get(pk=pk)
         book.delete()
-    return redirect('book_list')
+    return redirect('courses/book_list')
 
 
 class BookListView(ListView):
     model = Book
-    template_name = 'class_book_list.html'
+    template_name = 'courses/class_book_list.html'
     context_object_name = 'books'
 
 
 class UploadBookView(CreateView):
     model = Book
     form_class = BookForm
-    success_url = reverse_lazy('class_book_list')
-    template_name = 'upload_book.html'
+    success_url = reverse_lazy('courses/class_book_list')
+    template_name = 'courses/upload_book.html'
