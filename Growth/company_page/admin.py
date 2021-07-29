a90454aea256def8341c192bd31065902579f115
NamanBhandari26 <81213025+NamanBhandari26@users.noreply.github.com>
2021-07-29 22:08:11 +0530

    GROW:35 Deleted autogenerated files

--- Growth/company_page/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Company, File, Photo
-
-admin.site.register(Company)
-admin.site.register(File)
-admin.site.register(Photo)
-
