914ed3cbf6efc446c0cb349c66eeada7b8dfeeed
theRealEthanZhang <68260803+theRealEthanZhang@users.noreply.github.com>
2021-07-30 21:15:55 -0400

    GROW-98 finished ui for chatroom

--- Growth/video_chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import peer, peerA, peerB
-
-urlpatterns = [
-    url(r'^$', peer, name='peer'),
-    url(r'^peerA/', peerA, name='peerA'),
-    url(r'^peerB/', peerB, name='peerB'),
-]
